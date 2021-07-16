b869868b16732d60b8bfdf6a230308676ca65e27
Sergi de la Fuente <serg.dlf@gmail.com>
2021-07-16 19:24:17 +0200

    Explorer

--- src/viperplayer/Base.java
+++ src/viperplayer/Base.java
@@ -5,21 +5,28 @@ import aic2021.user.*;
 public class Base extends MyUnit {
 
     int workers = 0;
+    int explorers = 0;
+    int waterTiles = 0;
 
     Base(UnitController uc){
         super(uc);
     }
 
     void playRound(){
-        this.research();
+        research();
+
+        if(uc.getRound() == 1) senseInitialWater();
 
         UnitInfo[] enemies = uc.senseUnits(uc.getTeam().getOpponent());
         Location baseLoc = uc.getLocation();
 
-        this.baseAttack(enemies, baseLoc);
+        baseAttack(enemies, baseLoc);
 
         if (workers < 5){
             if (spawnRandom(UnitType.WORKER)) ++workers;
+        }
+        if (explorers < 1 && workers > 2) {
+            if (spawnRandom(UnitType.EXPLORER)) ++explorers;
         }
     }
 
@@ -36,6 +43,7 @@ public class Base extends MyUnit {
 
     private void research(){
         if(uc.canResearchTechnology(Technology.COIN)) uc.researchTechnology(Technology.COIN);
+        else if(uc.canResearchTechnology(Technology.RAFTS) && this.waterTiles > 4) uc.researchTechnology(Technology.RAFTS);
         else if(uc.canResearchTechnology(Technology.BOXES)) uc.researchTechnology(Technology.BOXES);
         else if(uc.canResearchTechnology(Technology.UTENSILS)) uc.researchTechnology(Technology.UTENSILS);
         else if(uc.canResearchTechnology(Technology.EUGENICS)) uc.researchTechnology(Technology.EUGENICS);
@@ -44,6 +52,13 @@ public class Base extends MyUnit {
         else if(uc.canResearchTechnology(Technology.POISON)) uc.researchTechnology(Technology.POISON);
         else if(uc.canResearchTechnology(Technology.SCHOOLS)) uc.researchTechnology(Technology.SCHOOLS);
         else if(uc.canResearchTechnology(Technology.WHEEL)) uc.researchTechnology(Technology.WHEEL);
+    }
+
+    private void senseInitialWater() {
+        Location[] initialWaterTiles = uc.senseWater(uc.getType().getVisionRange());
+        for(int i=0; i<initialWaterTiles.length; i++) {
+            this.waterTiles++;
+        }
     }
 
 }
--- src/viperplayer/Explorer.java
+++ src/viperplayer/Explorer.java
@@ -1,6 +1,6 @@
 package viperplayer;
 
-import aic2021.user.UnitController;
+import aic2021.user.*;
 
 public class Explorer extends MyUnit {
 
@@ -8,8 +8,52 @@ public class Explorer extends MyUnit {
         super(uc);
     }
 
-    void playRound(){
+    String state = "INI";
+    int turnsStopped = 0;
+    Location lastLoc;
 
+    Team myTeam = uc.getTeam();
+    Location baseLocation;
+
+    Location getBaseLocation(){
+        UnitInfo[] units = uc.senseUnits(2, myTeam);
+        for (UnitInfo unit:units){
+            if (unit.getType()== UnitType.BASE){
+                return unit.getLocation();
+            }
+        }
+        return new Location(-1, -1);
+    }
+
+    void playRound(){
+        //move.moveTo(enemyBaseLoc, false, this::moveCircle);
+        if (state == "INI"){
+            baseLocation = getBaseLocation();
+            state = "EXPLORE";
+        }
+        // Explore for resources
+        if (state == "EXPLORE") {
+            move.explore(false);
+            Location myNewLoc = uc.getLocation();
+            if (myNewLoc.isEqual(lastLoc)) {
+                turnsStopped++;
+                if (turnsStopped > 4) state = "RETURN";
+            } else {
+                lastLoc = myNewLoc;
+                turnsStopped = 0;
+            }
+        }
+        // Explore for resources
+        if (state == "RETURN") {
+            move.moveTo(baseLocation, false);
+            if (uc.getLocation().distanceSquared(baseLocation) < 2) {
+                state = "EXPLORE";
+            }
+        }
+    }
+
+    boolean moveCircle(Direction dir) {
+        return uc.canMove(dir) && uc.getLocation().add(dir).distanceSquared(baseLocation) <= (10 + uc.getRound() / 10);
     }
 
 }
