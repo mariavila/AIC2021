7591c87208dc75ef7bf4a9f4c02e511ffb8b66df
Erik Martinez <emrbeltane@gmail.com>
2021-08-02 17:20:28 +0200

    Change constants just in case

--- src/eduardkhil/Constants.java
+++ src/eduardkhil/Constants.java
@@ -3,18 +3,18 @@ package eduardkhil;
 public class Constants {
     // MAX constant is 1944
 
-    public int ALLY_BASE = 3;
-    public int ENEMY_BASE = 5;
-    public int RUSH_ATTACK_ENCODING = 7;
-    public int ENEMY_FOUND = 11;
-    public int BARRACKS_BUILT = 13;
-    public int BARRACKS_ALIVE = 17;
-    public int UNDER_ATTACK = 19;
-    public int ECO_MAP = 23;
-    public int SETTLEMENT = 29;
-    public int WATER = 31;
-    public int ATTACK_BASE = 37;
-    public int ENEMY_BARRACKS = 41;
-    public int ROUND_CHECK_ATTACK = 43;
-    public int ROUND_STOP_SOLDIERS = 47;
+    public int ALLY_BASE = 157;
+    public int ENEMY_BASE = 163;
+    public int RUSH_ATTACK_ENCODING = 167;
+    public int ENEMY_FOUND = 173;
+    public int BARRACKS_BUILT = 179;
+    public int BARRACKS_ALIVE = 181;
+    public int UNDER_ATTACK = 191;
+    public int ECO_MAP = 193;
+    public int SETTLEMENT = 197;
+    public int WATER = 199;
+    public int ATTACK_BASE = 211;
+    public int ENEMY_BARRACKS = 223;
+    public int ROUND_CHECK_ATTACK = 227;
+    public int ROUND_STOP_SOLDIERS = 229;
 }
