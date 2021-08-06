a98e88a6da0a9c66bbd6737ca640a71a16bb79cd
Erik Martinez <emrbeltane@gmail.com>
2021-08-06 15:49:16 +0200

    Added extra 7

---
+++ src/extra7/Sawmill.java
@@ -0,0 +1,15 @@
+package extra7;
+
+import aic2021.user.UnitController;
+
+public class Sawmill extends MyUnit {
+
+    Sawmill(UnitController uc){
+        super(uc);
+    }
+
+    void playRound(){
+
+    }
+
+}
